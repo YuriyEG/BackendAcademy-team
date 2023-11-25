f2826e390d7903997413ba0e90e9152fc705ba48
YuriyEG <you@example.com>
2023-11-25 09:24:40 +0300

    first: a basic adaptive mesh has been created

---
+++ src/components/App/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import styles from './App.module.css';
+
+export const App = () => {
+  return (
+    <div className={styles.app}>
+      <div className={styles.app__header}></div>
+      <div className={styles.app__main}>
+        <div className={styles.app__aside}>
+          <div>sfsdfs</div>
+        </div>
+        <div className={styles.app__content}>
+          dfsdfsdfsdfsdfdf
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+          <div>sfsdfs</div>
+        </div>
+      </div>
+      <div className={styles.app__footer}></div>
+    </div>
+  );
+};
+
+export default App;
